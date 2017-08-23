0f87113369ca5da46d02a28fc8790230ce58c841
Paras Sidhu <sidhuparas1234@gmail.com>
2017-08-23 22:01:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 var counter = 0;
 app.get('/counter', function(req,res){
     counter = counter+1;
