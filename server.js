a6315159a78ef7fa82abefdd129caf08659471f6
Paras Sidhu <sidhuparas1234@gmail.com>
2017-08-24 12:11:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.get('/counter', function(req,res){
 });
 
 var names = [];
-app.get('/submit-name/:name', function(req,res){
+app.get('/submit-name', function(req,res){
    
    var name = req.params.name;
    names.push(name);
