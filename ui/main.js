9078e70ebf754a04a5e5fbe47b9cb060efed8629
Paras Sidhu <sidhuparas1234@gmail.com>
2017-08-24 12:07:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,3 +17,10 @@ button.onclick = function(){
   request.open('GET','http://sidhuparas1234.imad.hasura-app.io/counter',true);
   request.send(null);
 };
+
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+    
+};
