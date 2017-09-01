443f5d960f9776951f337f8b70db548c49bb340c
Paras Sidhu <sidhuparas1234@gmail.com>
2017-09-01 12:45:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+const { Pool, Client } = require('pg');
+
+var config ={
+    user:'sidhuparas1234',
+    database: 'sidhuparas1234',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: password.env.DB_PASSWORD
+};
+
 var app = express();
 app.use(morgan('combined'));
 
@@ -12,6 +22,18 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+
+
+var pool = new Pool(config);
+app.get('/test-db'), function(req, res){
+    pool.query("SELECT * FROM test", function(err, result){
+       if(err) {
+           res.status(500).send(err.toString());
+       }else{
+           res.send(JSON.stringify(result));
+       }
+    });
+};
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
