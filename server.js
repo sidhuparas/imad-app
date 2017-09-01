ff97108cef4562ff045ce762275ae66ef5cf70f0
Paras Sidhu <sidhuparas1234@gmail.com>
2017-09-01 12:47:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var config ={
     database: 'sidhuparas1234',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: password.env.DB_PASSWORD
+    password: process.env.DB_PASSWORD
 };
 
 var app = express();
