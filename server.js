941f41cc93ed35ae9717bcf8fee6c72ea2bd6d34
Paras Sidhu <sidhuparas1234@gmail.com>
2017-09-02 11:17:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/', function (req, res) {
 });
 
 function hash(input, salt){
-    var hashed = crypto.pbkdf2Sync(input, salt,10000,512,'sha512');
+    var hashed = crypto.pbkdf2Sync(input, salt,10000,20,'sha512');
     return hashed.toString('hex');
 }
 
