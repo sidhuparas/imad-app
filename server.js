4c6b77429718302fb7b1c834a5f4e9b7f939262f
Paras Sidhu <sidhuparas1234@gmail.com>
2017-09-02 11:14:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 
 var config ={
     user:'sidhuparas1234',
@@ -16,6 +17,18 @@ app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+function hash(input, salt){
+    var hashed = crypto.pbkdf2Sync(input, salt,10000,512,'sha512');
+    return hashed.toString('hex');
+}
+
+var salt = 'paras-sidhu';
+
+app.get('/hash/:input', function(req,res){
+   var hashedString = hash(req.params.input, salt);
+   res.send(hashedString);
 });
 
 app.get('/ui/style.css', function (req, res) {
