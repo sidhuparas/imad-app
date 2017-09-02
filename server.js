b32b44c5094b9bd637eda58dc621345aafb8d89f
Paras Sidhu <sidhuparas1234@gmail.com>
2017-09-02 16:56:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,6 +19,22 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+app.post('/create-user', function(req, res){
+    var username = req.body.username;
+    var password = req.body.password;
+    var salt = crypto.randomBytes(128).toString('hex');
+    var dbString = hash(password, salt);
+    
+    pool.query('INSERT INTO users (username, password) VALUES ($1, $2)',[username, dbString], function (err, result){
+        if(err){
+            res.status(500).send(err.toString());
+        } else{
+            res.send('User successfully created: '+username);
+        }
+    });
+});
+
 function hash(input, salt){
     var hashed = crypto.pbkdf2Sync(input, salt,10000,20,'sha512');
     return hashed.toString('hex');
